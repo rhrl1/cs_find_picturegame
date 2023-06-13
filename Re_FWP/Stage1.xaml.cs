d4d7b41f5f64815ab7b80bed5f77b10af8c91562
rhrl1 <127263684+rhrl1@users.noreply.github.com>
2023-06-13 20:05:13 +0900

    Add files via upload

---
+++ Re_FWP/Stage1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace Re_FWP
+{
+    /// <summary>
+    /// Stage1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Stage1 : Page
+    {
+        public Stage1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Stage2.xaml", UriKind.Relative));
+        }
+
+        
+    }
+}
